9404d0f3d5d0bdc675eb7e217b1601a4001b0a03
JaeW <janice.wildrick@gmail.com>
2017-02-16 01:44:09 -0600

    Added CoffeeContract - contract for SQLite db containing record of coffee consumption

---
+++ app/src/main/java/com/nemwick/coffeetrack/data/CoffeeContract.java
@@ -0,0 +1,33 @@
+package com.nemwick.coffeetrack.data;
+
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class CoffeeContract {
+    public static final String CONTENT_AUTHORITY = "com.nemwick.coffeetrack";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_COFFEE = "coffee";
+
+    private CoffeeContract() {
+        // private to prevent instantiation - singleton
+    }
+
+    public static abstract class CoffeeEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_COFFEE).build();
+
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/" + CONTENT_URI + "/" + PATH_COFFEE;
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/" + CONTENT_URI + "/" + PATH_COFFEE;
+
+        public static final String TABLE_NAME = "coffee";
+        //primary key auto-incrementing
+        public static final String _ID = BaseColumns._ID;
+        /* date/time coffee was consumed */
+        public static final String COLUMN_COFFEE_TIME = "time";
+
+        public static Uri buildCoffeeUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+}
