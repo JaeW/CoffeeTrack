78676cd15f375ed6a1eebe8513ab4fa87ce7a8c2
JaeW <janice.wildrick@gmail.com>
2017-02-21 18:19:00 -0600

    instantiate FAB & add click listener

--- app/src/main/java/com/nemwick/coffeetrack/MainActivity.java
+++ app/src/main/java/com/nemwick/coffeetrack/MainActivity.java
@@ -1,13 +1,25 @@
 package com.nemwick.coffeetrack;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+    private View.OnClickListener fabClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //TODO:  add new coffee to CoffeeContract
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(fabClickListener);
     }
 }
