149a242d8746af5d4f8452e2edd6e7faf22caf4f
JaeW <janice.wildrick@gmail.com>
2017-02-27 20:24:18 -0600

    Implemented abstract CursorLoader methods Retrieve cursor and tally how many cups of coffee drank within day, week, month populate TextViews

--- app/src/main/java/com/nemwick/coffeetrack/CoffeeStatsActivity.java
+++ app/src/main/java/com/nemwick/coffeetrack/CoffeeStatsActivity.java
@@ -12,11 +12,16 @@ import android.widget.TextView;
 
 import com.nemwick.coffeetrack.data.CoffeeContract;
 
+import java.util.Calendar;
+
 public class CoffeeStatsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     private TextView dayCupsCoffee;
     private TextView weekCupsCoffee;
     private TextView monthCupsCoffee;
     public static final int LOADER_ID = 222;
+    public static final int DAY = 1;
+    public static final int WEEK = 7;
+    public static final int MONTH = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,27 +38,47 @@ public class CoffeeStatsActivity extends AppCompatActivity implements LoaderMana
 
         //cursorloader initialization
         getSupportLoaderManager().initLoader(LOADER_ID, null, this);
-
-
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Long oneMonthAgo = this.calculatePriorDateInMillis(MONTH);
         Uri queryUri = CoffeeContract.CoffeeEntry.CONTENT_URI;
-        String[] queryProjection = {CoffeeContract.CoffeeEntry.COLUMN_COFFEE_TIME};
-        //TODO;  Specify query to retrieve only last month's coffee records
-        return new CursorLoader(this, queryUri, queryProjection, null, null, null);
+        String[] projection = {CoffeeContract.CoffeeEntry.COLUMN_COFFEE_TIME};
+        String selection = CoffeeContract.CoffeeEntry.COLUMN_COFFEE_TIME + " >= ?";
+        String[] selectionArgs = {Long.toString(oneMonthAgo)};
+        String sortOrder = CoffeeContract.CoffeeEntry.COLUMN_COFFEE_TIME + " ASC";
+        return new CursorLoader(this, queryUri, projection, selection, selectionArgs, sortOrder);
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        //TODO:  count cursor records of each type (day, week month) and populate TextViews
+        int monthTotal = data.getCount();
+        int weekTotal = 0;
+        int dayTotal = 0;
+        long weekInMillis = this.calculatePriorDateInMillis(WEEK); //Unix date in millis for one week prior to today
+        long dayInMillis = this.calculatePriorDateInMillis(DAY); //Unix date in millis for one day prior to today
 
+            while (data.moveToNext()) {
+                long tempTimeValue = data.getLong(data.getColumnIndex(CoffeeContract.CoffeeEntry.COLUMN_COFFEE_TIME));
+                if (weekInMillis <= tempTimeValue) { weekTotal++;}  //if coffee drunk after one week ago timestamp, increment
+                if(dayInMillis <= tempTimeValue){dayTotal++;} //if coffee drunk after one day ago timestampe, increment
+            }
+
+        this.monthCupsCoffee.setText(Long.toString(monthTotal));
+        this.weekCupsCoffee.setText(Long.toString(weekTotal));
+        this.dayCupsCoffee.setText(Long.toString(dayTotal));
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
 
+    }
+
+    private long calculatePriorDateInMillis(int days) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.DATE, c.get(Calendar.DATE) - days);
+        return c.getTimeInMillis();
     }
 
 }
