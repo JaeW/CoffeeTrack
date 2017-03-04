8e05b33a4230eb71b899982e2d99310316ee8ed6
JaeW <janice.wildrick@gmail.com>
2017-03-03 21:50:21 -0600

    Added basic functionality to app widget (displays but not functionality).

--- app/src/main/java/com/nemwick/coffeetrack/CoffeeWidgetProvider.java
+++ app/src/main/java/com/nemwick/coffeetrack/CoffeeWidgetProvider.java
@@ -1,12 +1,32 @@
 package com.nemwick.coffeetrack;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
 
 
 public class CoffeeWidgetProvider extends AppWidgetProvider {
 
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        ComponentName me = new ComponentName(context, CoffeeWidgetProvider.class);
+        appWidgetManager.updateAppWidget(me, buildUpdate(context, appWidgetIds));
+
+    }
+
+    private RemoteViews buildUpdate(Context context, int[] appWidgetIds) {
+
+        RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.widget_coffee);
+        //Intent i = new Intent(context, CoffeeWidgetProvider.class);
 
 
 
+        return updateViews;
+    }
 
 }
