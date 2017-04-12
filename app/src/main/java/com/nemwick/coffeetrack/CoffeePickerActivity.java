63d3c06e82491664a03be1f864601290fa48d8e5
JaeW <janice.wildrick@gmail.com>
2017-04-11 22:20:49 -0500

    Added permission request for M and above

--- app/src/main/java/com/nemwick/coffeetrack/CoffeePickerActivity.java
+++ app/src/main/java/com/nemwick/coffeetrack/CoffeePickerActivity.java
@@ -1,12 +1,18 @@
 package com.nemwick.coffeetrack;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
@@ -16,6 +22,7 @@ import com.google.android.gms.location.places.ui.PlacePicker;
 public class CoffeePickerActivity extends AppCompatActivity {
 
     private static final int PLACE_PICKER_REQUEST = 1;
+    private final static int MY_PERMISSION_FINE_LOCATION = 101;
     private Button findCoffeeBtn;
     private TextView coffeeShopName;
     private TextView coffeeShopAddy;
@@ -60,10 +67,35 @@ public class CoffeePickerActivity extends AppCompatActivity {
             coffeeShopAddy.setText(place.getAddress());
 
             if (place.getAttributions() == null) {
-                attribution.loadData("no attribution", "text/html; charset=utf-8", "UFT-8" );
-            } else{
+                attribution.loadData("no attribution", "text/html; charset=utf-8", "UFT-8");
+            } else {
                 attribution.loadData(place.getAttributions().toString(), "text/html; charset=utf-8", "UFT-8");
             }
         }
     }
+
+    private void requestPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSION_FINE_LOCATION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case MY_PERMISSION_FINE_LOCATION:
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "This app requires location permissions to be granted", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
 }
